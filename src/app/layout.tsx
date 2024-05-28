785644ed98a08ba7abeeb6a7f2fb591fb0149f7c
Probir Sarkar <Probirsarkar855@gmail.com>
2024-05-28 12:22:05 +0530

    added og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,16 @@ export const metadata: Metadata = {
   title: "Probir Sarkar - Full Stack Developer",
   description:
     "Explore the expertise of Probir Sarkar, an accomplished Full Stack Developer with a focus on Next.js. With a wealth of experience, I bring a proven track record of creating robust and dynamic web applications. Dive into my portfolio to witness the seamless integration of front-end and back-end technologies, showcasing a commitment to excellence in every project. ",
+  openGraph: {
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Probir Sarkar - Full Stack Developer",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
